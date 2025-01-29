0a0d169dae1203af4ee4e83ea24cd2d4cb5879ee
ahushv3432 <andreyriabov67@gmail.com>
2025-01-29 14:58:47 +0200

    Add files via upload

---
+++ Спам бот для телеграмм  Рассылка спама телеграмм/config.py
@@ -0,0 +1,10 @@
+
+#BOT:
+TOKEN = "7611500472:AAFJpPwAvxz56OZBxKc_X2ptQ9ydeHuUhbM" # ТОКЕН БОТА
+ADMIN = 6803274033 # ИД АДМИНА, узнать свой айди - @get_any_telegram_id_bot
+
+DIR = "" # Путь к файлу, если нужно
+
+#USER: # https://my.telegram.org/auth
+API_ID = 29004686
+API_HASH = "1429956a31ddd9a9fdeeab0498e0a528"
